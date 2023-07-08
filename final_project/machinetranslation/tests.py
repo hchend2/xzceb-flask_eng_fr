4367e2a141e64d695a473a6ff6bb3968771946ac
hchend2 <hchend2@uic.edu>
2023-07-08 14:44:55 -0400

    some changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,12 +4,20 @@ from translator import english_to_french
 from translator import french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
-    def test(sefl):
+    def test1(sefl):
         sefl.assertEqual(english_to_french("Hello"), "Bonjour")
+        
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test2(sefl):
         sefl.assertNotEqual(english_to_french("Tank you"), "Moi")
 
 
 class TestFrenchToEnglish(unittest.TestCase):
-    def test(sefl):
+    def test1(sefl):
         sefl.assertEqual(french_to_english("Bonjour"), "Hello")
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test2(sefl):
         sefl.assertNotEqual(french_to_english("Dieu"), "Me")
